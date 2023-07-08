b519a4062879e03b3235fd841147cc4fde3ba761
Simao Gama <gama.simao@gmail.com>
2023-07-08 16:27:43 +0100

    added themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+// import { useContext } from 'react';
+import LandingPage from './pages/Home/LandingPage';
+import SignUp from './pages/Signup/Signup';
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>MY APP</div>
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </>
   );
 }
