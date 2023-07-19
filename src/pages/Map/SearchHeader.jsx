27dff939fe1147b48bbcc0761cf231206cf97520
Simao Gama <gama.simao@gmail.com>
2023-07-19 21:59:15 +0100

    changes and deploy

--- src/pages/Map/SearchHeader.jsx
+++ src/pages/Map/SearchHeader.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from "react";
-import { Autocomplete } from "@react-google-maps/api";
-import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { styled } from "@mui/system";
+import React, { useState } from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled } from '@mui/system';
 
 const SearchContainer = styled(Box)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
+  display: 'flex',
+  alignItems: 'center',
   backgroundColor: theme.palette.common.white,
   borderRadius: theme.shape.borderRadius,
-  "&:hover": {
-    backgroundColor: theme.palette.common.white,
+  '&:hover': {
+    backgroundColor: theme.palette.common.white
   },
   marginLeft: theme.spacing(2),
-  width: "auto",
+  width: 'auto'
 }));
 
-const SearchIconContainer = styled("div")(({ theme }) => ({
+const SearchIconContainer = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: "100%",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  color: theme.palette.primary.main,
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.primary.main
 }));
 
 const Input = styled(InputBase)(({ theme }) => ({
-  color: "black",
+  color: 'black',
   padding: theme.spacing(1, 1, 1, 0),
   paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-  transition: theme.transitions.create("width"),
-  width: "100%",
+  transition: theme.transitions.create('width'),
+  width: '100%'
 }));
 
 const SearchHeader = ({ setCoordinates }) => {
   const [autocomplete, setAutocomplete] = useState(null);
 
-  const onLoad = (autoC) => setAutocomplete(autoC);
+  const onLoad = autoC => setAutocomplete(autoC);
 
   const onPlaceChanged = () => {
     const place = autocomplete.getPlace();
@@ -60,7 +60,7 @@ const SearchHeader = ({ setCoordinates }) => {
               <Input
                 placeholder="Search…"
                 classes={{
-                  input: "MuiInputBase-input",
+                  input: 'MuiInputBase-input'
                 }}
               />
             </SearchContainer>
