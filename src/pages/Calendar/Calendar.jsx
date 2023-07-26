8055078340672001b18b99251f25c558f1f9d822
Simao Gama <gama.simao@gmail.com>
2023-07-26 20:15:05 +0100

    changes

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import { useState, useEffect } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 // import { eventDrop } from "@fullcalendar/interaction";
 
-import listPlugin from "@fullcalendar/list";
+import listPlugin from '@fullcalendar/list';
 import {
   Box,
   List,
@@ -14,42 +14,42 @@ import {
   Typography,
   Modal,
   Button,
-  ThemeProvider,
-} from "@mui/material";
-import ColorLensTwoToneIcon from "@mui/icons-material/ColorLensTwoTone";
-import { styled, useTheme } from "@mui/material/styles";
-import { useContext } from "react";
-import { AuthContext } from "../../context/auth.context";
-import useFetch from "../../hooks/useFetch";
-import { addTrip, baseURL, updateTrip, deleteTrip } from "../../api/trips.api";
-import { CompactPicker } from "react-color";
-import { TripColorsContext } from "../../context/tripColors.context";
-import NewTrip from "../../components/CreateTrip/NewTrip";
-import { tokens } from "../../context/theme.context";
-import { Link, useNavigate } from "react-router-dom";
-import EventOptionsModal from "../../components/Modal/MultiSelectModal";
-import EditTrip from "../../components/EditTrip/EditTrip";
+  ThemeProvider
+} from '@mui/material';
+import ColorLensTwoToneIcon from '@mui/icons-material/ColorLensTwoTone';
+import { styled, useTheme } from '@mui/material/styles';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/auth.context';
+import useFetch from '../../hooks/useFetch';
+import { addTrip, baseURL, updateTrip, deleteTrip } from '../../api/trips.api';
+import { CompactPicker } from 'react-color';
+import { TripColorsContext } from '../../context/tripColors.context';
+import NewTrip from '../../components/CreateTrip/NewTrip';
+import { tokens } from '../../context/theme.context';
+import { Link, useNavigate } from 'react-router-dom';
+import EventOptionsModal from '../../components/Modal/MultiSelectModal';
+import EditTrip from '../../components/EditTrip/EditTrip';
 
 const SidebarBox = styled(Box)(({ theme }) => ({
-  flex: "1 1 20%",
+  flex: '1 1 20%',
   backgroundColor: theme.palette.primary.secondary,
   color: theme.palette.primary.contrastText,
-  padding: "15px",
-  borderRadius: "4px",
+  padding: '15px',
+  borderRadius: '4px'
 }));
 
 const SidebarTitle = styled(Typography)({
-  marginBottom: "10px",
+  marginBottom: '10px'
 });
 
 const ListItemStyled = styled(ListItem)(({ theme }) => ({
-  backgroundColor: "rgba(26,37,46)",
-  margin: "10px 0",
+  backgroundColor: 'rgba(26,37,46)',
+  margin: '10px 0',
   //   justifyContent: "center",
-  borderRadius: "2px",
-  "&:hover": {
-    backgroundColor: "rgb(44,62,80)",
-  },
+  borderRadius: '2px',
+  '&:hover': {
+    backgroundColor: 'rgb(44,62,80)'
+  }
 }));
 
 function Calendar() {
@@ -74,7 +74,7 @@ function Calendar() {
     data: allTrips,
     isLoading,
     error,
-    reFetch,
+    reFetch
   } = useFetch(`${baseURL}/trips?userId=${user._id}`);
 
   // console.log(allTrips);
@@ -83,7 +83,7 @@ function Calendar() {
   useEffect(() => {
     // Convert the fetched trips to events and set them to currentEvents
     const tripsAsEvents = allTrips
-      ? allTrips.map((trip) => {
+      ? allTrips.map(trip => {
           const savedColor = tripColors[trip._id];
           const days = trip.days.length; // Get the number of days for the trip
 
@@ -98,8 +98,8 @@ function Calendar() {
             title: trip.destination,
             start: formatDateToISO(trip.startDate),
             end: formatDateToISO(endDate),
-            backgroundColor: savedColor || "#4cceac",
-            borderColor: savedColor || "#4cceac",
+            backgroundColor: savedColor || '#4cceac',
+            borderColor: savedColor || '#4cceac'
           };
         })
       : [];
@@ -108,7 +108,7 @@ function Calendar() {
 
   // Load trip colors from localStorage when the component mounts
   useEffect(() => {
-    const savedTripColors = localStorage.getItem("tripColors");
+    const savedTripColors = localStorage.getItem('tripColors');
     if (savedTripColors) {
       setTripColors(JSON.parse(savedTripColors));
     }
@@ -116,12 +116,12 @@ function Calendar() {
 
   // Save trip colors to localStorage when tripColors state changes
   useEffect(() => {
-    localStorage.setItem("tripColors", JSON.stringify(tripColors));
+    localStorage.setItem('tripColors', JSON.stringify(tripColors));
   }, [tripColors]);
 
-  const handleDateClick = (selected) => {
+  const handleDateClick = selected => {
     // Prompt to get the title and number of days for the new trip
-    const title = prompt("Please enter a title for your trip");
+    const title = prompt('Please enter a title for your trip');
 
     // Validate input for title
     if (title) {
@@ -136,15 +136,15 @@ function Calendar() {
         startDate: selected.startStr,
         endDate: formatDateToISO(
           new Date(selected.end.getTime() - 24 * 60 * 60 * 1000)
-        ), // Adjusted end date
+        ) // Adjusted end date
       };
 
       // Add the new trip to the database using the addTrip function
       addTrip(newTrip, user._id)
-        .then((response) => {
+        .then(response => {
           // If the trip was successfully added to the database, update the calendar view
           const { data: newTripData } = response;
-          const savedColor = tripColors[newTripData._id] || "#4cceac";
+          const savedColor = tripColors[newTripData._id] || '#4cceac';
 
           const event = {
             id: newTripData._id,
@@ -152,24 +152,25 @@ function Calendar() {
             start: newTripData.startDate,
             end: new Date(newTripData.endDate), // Use a Date object for the end date
             backgroundColor: savedColor,
-            borderColor: savedColor,
+            borderColor: savedColor
           };
 
-          setCurrentEvents((prevEvents) => [...prevEvents, event]);
+          setCurrentEvents(prevEvents => [...prevEvents, event]);
           reFetch();
         })
-        .catch((error) => {
-          console.error("Error adding trip:", error);
+        .catch(error => {
+          console.error('Error adding trip:', error);
         });
     }
   };
 
   // Edit trip to the database using the updateTrip function
-  const handleEventDrop = (dropInfo) => {
+  const handleEventDrop = dropInfo => {
     const { event } = dropInfo;
     const { id: tripId, start, end } = event;
 
     const adjustedEndDate = new Date(end.getTime() - 24 * 60 * 60 * 1000);
+    // const adjustedEndDate = new Date(end)
 
     // Format the start and adjusted end dates in ISO format
     const formattedStartDate = formatDateToISO(start);
@@ -181,38 +182,38 @@ function Calendar() {
     const updatedTrip = {
       destination, // Keep the destination the same
       startDate: formattedStartDate,
-      endDate: formattedEndDate,
+      endDate: formattedEndDate
     };
 
-    console.log("updatedTrip:", updatedTrip);
+    console.log('updatedTrip:', updatedTrip);
 
     // Call the updateTrip function to update the trip in the database
     updateTrip(updatedTrip, tripId)
       .then(() => {
         // If the trip was successfully updated in the database, update the currentEvents state
-        setCurrentEvents((prevEvents) =>
-          prevEvents.map((prevEvent) =>
+        setCurrentEvents(prevEvents =>
+          prevEvents.map(prevEvent =>
             prevEvent.id === tripId
               ? {
                   ...prevEvent,
                   title: destination, // Update the title (destination) in the event
                   start: formattedStartDate,
-                  end: adjustedEndDate, // Use the adjusted end date
+                  end: adjustedEndDate // Use the adjusted end date
                 }
               : prevEvent
           )
         );
         reFetch();
       })
-      .catch((error) => {
-        console.error("Error updating trip:", error);
+      .catch(error => {
+        console.error('Error updating trip:', error);
       });
   };
 
-  const handleEventClick = (clickInfo) => {
+  const handleEventClick = clickInfo => {
     // Get the selected trip from the currentEvents state based on the ID of the clicked event
     const selectedTrip = currentEvents.find(
-      (event) => event.id === clickInfo.event.id
+      event => event.id === clickInfo.event.id
     );
 
     // Set the selectedTrip to the state to show the modal
@@ -227,13 +228,13 @@ function Calendar() {
         .then(() => {
           // If the trip is deleted successfully, remove the event from the calendar
           const updatedEvents = currentEvents.filter(
-            (event) => event.id !== selectedTrip.id
+            event => event.id !== selectedTrip.id
           );
           setCurrentEvents(updatedEvents);
           setSelectedTrip(null);
         })
-        .catch((error) => {
-          console.error("Error deleting trip:", error);
+        .catch(error => {
+          console.error('Error deleting trip:', error);
           setSelectedTrip(null);
         });
     }
@@ -257,35 +258,35 @@ function Calendar() {
   function formatDateToISO(date) {
     const dateObj = new Date(date);
     const year = dateObj.getUTCFullYear();
-    const month = String(dateObj.getUTCMonth() + 1).padStart(2, "0");
-    const day = String(dateObj.getUTCDate()).padStart(2, "0");
+    const month = String(dateObj.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(dateObj.getUTCDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
   }
 
   const handleColorChange = (tripId, color) => {
     const updatedTripColors = {
       ...tripColors,
-      [tripId]: color.hex,
+      [tripId]: color.hex
     };
 
     // Update the tripColors state with the updatedTripColors variable
     setTripColors(updatedTripColors);
 
     // Update the currentEvents state to reflect the new color for the trip
-    setCurrentEvents((prevEvents) =>
-      prevEvents.map((event) =>
+    setCurrentEvents(prevEvents =>
+      prevEvents.map(event =>
         event.id === tripId
           ? {
               ...event,
               backgroundColor: color.hex,
-              borderColor: color.hex,
+              borderColor: color.hex
             }
           : event
       )
     );
   };
 
-  const handleOpenColorPicker = (tripId) => {
+  const handleOpenColorPicker = tripId => {
     setSelectedTrip(tripId);
     setColorPickerVisible(true);
   };
@@ -297,7 +298,7 @@ function Calendar() {
 
   const handleShowNewTripForm = () => {
     setShowNewTripForm(!showNewTripForm);
-    console.log("here");
+    console.log('here');
   };
 
   return (
@@ -311,31 +312,31 @@ function Calendar() {
               <ListItemStyled
                 onClick={handleShowNewTripForm}
                 sx={{
-                  "&:hover, &.Mui-focusVisible": {
-                    backgroundColor: "#2da883", // Customize this color as needed
-                    cursor: "pointer",
-                  },
+                  '&:hover, &.Mui-focusVisible': {
+                    backgroundColor: '#2da883', // Customize this color as needed
+                    cursor: 'pointer'
+                  }
                 }}
               >
                 <ListItemText primary="Add New Trip" />
               </ListItemStyled>
-              {currentEvents.map((event) => (
+              {currentEvents.map(event => (
                 <ListItemStyled
                   key={event.id}
                   style={{ background: event.backgroundColor }}
                 >
                   <Link
                     to={`/trip/${event.id}`}
-                    style={{ textDecoration: "none", color: "black" }}
+                    style={{ textDecoration: 'none', color: 'black' }}
                   >
                     <ListItemText
                       primary={event.title}
                       secondary={
                         <Typography>
                           {formatDateToISO(event.start, {
-                            year: "numeric",
-                            month: "short",
-                            day: "numeric",
+                            year: 'numeric',
+                            month: 'short',
+                            day: 'numeric'
                           })}
                         </Typography>
                       }
@@ -346,14 +347,14 @@ function Calendar() {
                   <ColorLensTwoToneIcon
                     onClick={() => handleOpenColorPicker(event.id)}
                     sx={{
-                      color: "rgba(25,25,26,0.8)",
-                      fontSize: "2rem", // Adjust the size as needed
-                      marginLeft: "3rem", // Adjust the margin as needed
-                      cursor: "pointer",
-                      transition: "transform 0.2s ease-in-out", // Add a transition for zooming effect
-                      "&:hover": {
-                        transform: "scale(1.2)", // Apply a scale transformation on hover
-                      },
+                      color: 'rgba(25,25,26,0.8)',
+                      fontSize: '2rem', // Adjust the size as needed
+                      marginLeft: '3rem', // Adjust the margin as needed
+                      cursor: 'pointer',
+                      transition: 'transform 0.2s ease-in-out', // Add a transition for zooming effect
+                      '&:hover': {
+                        transform: 'scale(1.2)' // Apply a scale transformation on hover
+                      }
                     }}
                   />
                 </ListItemStyled>
@@ -369,12 +370,12 @@ function Calendar() {
                 dayGridPlugin,
                 timeGridPlugin,
                 interactionPlugin,
-                listPlugin,
+                listPlugin
               ]}
               headerToolbar={{
-                left: "prev,next today",
-                center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
               }}
               initialView="dayGridMonth"
               editable={true}
@@ -384,7 +385,7 @@ function Calendar() {
               select={handleDateClick}
               eventClick={handleEventClick}
               events={currentEvents}
-              eventColor={(info) => tripColors[info.event.id] || "#4cceac"}
+              eventColor={info => tripColors[info.event.id] || '#4cceac'}
               eventDrop={handleEventDrop}
             />
           </Box>
@@ -401,15 +402,15 @@ function Calendar() {
         <Modal open={colorPickerVisible} onClose={handleCloseColorPicker}>
           <Box
             sx={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)'
             }}
           >
             <CompactPicker
-              color={tripColors[selectedTrip] || "#ffffff"}
-              onChange={(color) => handleColorChange(selectedTrip, color)}
+              color={tripColors[selectedTrip] || '#ffffff'}
+              onChange={color => handleColorChange(selectedTrip, color)}
             />
           </Box>
         </Modal>
